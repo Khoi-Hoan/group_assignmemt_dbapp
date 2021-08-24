49f71ea6b9a7ce5307bfb575b4231e88aa4fab27
Khoi-Hoan <68211832+Khoi-Hoan@users.noreply.github.com>
2021-08-24 12:50:33 +0700

    the logic of register

--- register.php
+++ register.php
@@ -11,15 +11,22 @@ require_once('db.php')
     <div>
       <?php
       if(isset($_POST['create'])){
-        $email = $_POST['email']
-        $phone = $_POST['phone']
-        $firstname = $_POST['firstname']
-        $lastname = $_POST['lastname']
-        $password = $_POST['password']
-        $address = $_POST['address']
-        $city = $_POST['city']
-        $country = $_POST['country']
-        
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $id = $_POST['id'];
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $password = $_POST['password'];
+        $address = $_POST['address'];
+        $city = $_POST['city'];
+        $country = $_POST['country'];
+
+        $sql = "insert into customer (Customer_Email, Password, Phone, First_Name, Last_Name, Customer_ID, Address, City, Country) values (?,?,?,?,?,?,?,?,?)";
+        $stmt = $db -> prepare($sql);
+        $result = $stmt -> execute([$email, $password, $phone, $firstname, $lastname, $id, $address, $city, $country]);
+        if ($result){
+          echo 'Create an account Successfully'
+        }
       }
       ?>
     </div>
@@ -33,6 +40,9 @@ require_once('db.php')
 
           <p><label for="phone"><b>Phone Number</b></label>
           <input type="text" name="phone" required></p>
+
+          <p><label for="id"><b>National ID</b></label>
+          <input type="text" name="id" required></p>
 
           <p><label for="firstname"><b>First Name</b></label>
           <input type="text" name="firstname" required></p>
