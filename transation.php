230bdeeca9dbd77a4aae5ddec4aabb40ee1a73e5
Khoi-Hoan <68211832+Khoi-Hoan@users.noreply.github.com>
2021-09-09 01:40:52 +0700

    complete

--- transation.php
+++ transation.php
@@ -51,37 +51,53 @@ if ($_SESSION['User'] == 'admin'){
       $sql1 = "SELECT Balance FROM Customer WHERE Customer_Email = '$seller'";
       $stmt1 = $dbh->query($sql1);
       $row1 = $stmt1->fetch(PDO::FETCH_ASSOC);
-      echo $row1['Balance'];
 
       $sql2 = "SELECT MAX(Bid) AS amount FROM Bid WHERE Auction_ID = '$id'";
       $stmt2 = $dbh->query($sql2);
       $row2 = $stmt2->fetch(PDO::FETCH_ASSOC);
       $amount = $row2['amount'];
-      echo $row2['amount'];
 
       $sql3 = "SELECT Customer_Email AS buyer FROM Bid WHERE Auction_ID = '$id'  AND Bid = '$amount'";
       $stmt3 = $dbh->query($sql3);
       $row3 = $stmt3->fetch(PDO::FETCH_ASSOC);
       $buyer = $row3['buyer'];
-      echo $row3['buyer'];
 
       $sql4 = "SELECT Balance FROM Customer WHERE Customer_Email = '$buyer'";
       $stmt4 = $dbh->query($sql4);
       $row4 = $stmt4->fetch(PDO::FETCH_ASSOC);
-      echo $row4['Balance'];
 
-      $sql = "CALL sp_tranfer_money('$id','$seller')";
-      $stmt = $dbh->prepare($sql);
-      $res = $stmt->execute();
+      try {
+        $dbh->beginTransaction();
+        if ($amount > $row4['Balance']){
+          echo "Buyer account is too low";
+        }
+        else {
+          $sqlto = "UPDATE Customer SET Balance = Balance  + $amount WHERE Customer_Email = '$seller'";
+          $stmtto = $dbh->query($sqlto);
+          $resto= $stmtto->execute();
 
-      if ($res){
-        echo 'Tranfer successfully';
+          $sqlfrom = "UPDATE Customer SET Balance = Balance  - $amount WHERE Customer_Email = '$buyer'";
+          $stmtfrom = $dbh->prepare($sqlfrom);
+          $resfrom = $stmtfrom->execute();
+
+          $dbh->commit();
+
+          $sql5 = "SELECT Balance FROM Customer WHERE Customer_Email = '$buyer'";
+          $stmt5 = $dbh->query($sql5);
+          $row5 = $stmt5->fetch(PDO::FETCH_ASSOC);
+
+          $sql6 = "SELECT Balance FROM Customer WHERE Customer_Email = '$seller'";
+          $stmt6 = $dbh->query($sql6);
+          $row6 = $stmt6->fetch(PDO::FETCH_ASSOC);
+
+          echo $seller . " Balance: " . $row1['Balance'] . " => " . $row6['Balance'] . '<br>';
+          echo $buyer . " Balance: " . $row4['Balance'] . " => " . $row5['Balance'] . '<br>';
+        }
       }
-      else{
-        var_dump($res);
+      catch (Exception $e){
+        $dbh->rollBack();
+        die($e->getMessage());
       }
-
-
     }
   }
 
